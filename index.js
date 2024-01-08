945466792ac0c0e7e646d15c7b68f4a42fc447fe
Sarah <sarah.matta@enformion.com>
2024-01-08 12:57:48 -0500

    cleaned for Sonny

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-//feature 2
-//colors to make you feel more of whatever buttons you choose on the top
-//buttons are: happy, sad, angry, calm, excited, bored, anxious, tired, energized, etc
-//a toggled on button means the color palette displays a color "proven" to make you feel that way
-
-
-//todo try radial gradients with different shapes
-//todo sarah particles working when first clicked and no "on" colors
-//todo sarah why is particles doing what it's doing when I extra click?
-//todo sarah why doesn't the canvas clear between particles and others?
+//todo s Overlaying transparent shapes https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/globalAlpha
 //todo sarah make choice array? choice needs to be in the array?
 //todo add different options in the lineargradient function
 //todo sarah split into different js files, start with circle particles because it's big
-//todo check responsiveness
 
 let choice = 'sl';
 
@@ -50,7 +40,7 @@ function changeColor(element) {
 
   if (items[elem].on === true){
     items[elem].on = false;
-    element.style.backgroundColor = '#f5f5f5';
+    element.style.backgroundColor = '#d5dfed';
   } else {
     items[elem].on = true;
     element.style.backgroundColor = items[elem].color;
@@ -59,25 +49,9 @@ function changeColor(element) {
   renderCanvas();
 }
 
-//todo sarah make this a switch, or a loop through choices array
-//then can omit clickedFunctions
-function clickedStraightLines(){
-  choice = 'sl';
-  renderCanvas();
-}
-
-function clickedLinearGradient(){
-  choice = 'lg';
-  renderCanvas();
-}
-
-function clickedDiagonalGradient(){
-  choice = 'dg';
-  renderCanvas();
-}
-
-function clickedCircleParticles(){
-  choice = 'cp';
+function handleChoiceClicked(id){
+  choice = id;
+  console.log('id was ' + id + 'choice is now -->' + choice);
   renderCanvas();
 }
 
@@ -89,48 +63,54 @@ function renderCanvas(){
     case 'dg':
       renderDiagonalGradientCanvas();
       break;
+    case 'rg':
+      renderRadialGradientCanvas();
+      break;
+    case 'cg':
+      renderConicGradientCanvas();
+      break;
     case 'cp':
       renderCircleParticlesCanvas();
       break;
     default:
-        renderStraightLinesCanvas();
+      renderStraightLinesCanvas();
   }
+}
+
+function getContext(){
+  const canvas = document.getElementById('colorCanvas');
+  const ctx = canvas.getContext('2d');
+  const onItems = Object.values(items).filter(item => item.on);
+  // ctx.reset();
+  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = '#ffffff';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  return {canvas, ctx, onItems};
 }
 
 function renderStraightLinesCanvas(){
   console.log('rendering straight lines canvas');
 
-  const canvas = document.getElementById('colorCanvas');
-  const ctx = canvas.getContext('2d');
-  //ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const {canvas, ctx, onItems} = getContext();
 
-  const onItems = Object.values(items).filter(item => item.on);
-  const itemWidth = canvas.width / onItems.length;
+  if (onItems.length > 0) {
+    const itemWidth = canvas.width / onItems.length;
 
-  onItems.forEach((item, index) => {
-    if (!item.on) return;
-    ctx.fillStyle = item.color;
-    ctx.fillRect(index * itemWidth, 0, itemWidth, canvas.height);
-  });
+    onItems.forEach((item, index) => {
+      if (!item.on) return;
+      ctx.fillStyle = item.color;
+      ctx.fillRect(index * itemWidth, 0, itemWidth, canvas.height);
+    });
+  }
 }
 
 function renderLinearGradientCanvas(){
   console.log('rendering linear gradient canvas');
 
-  const canvas = document.getElementById('colorCanvas');
-  const ctx = canvas.getContext('2d');
-  //ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const {canvas, ctx, onItems} = getContext();
 
-  const onItems = Object.values(items).filter(item => item.on);
-
-  if (onItems.length === 0) {
-    ctx.fillStyle = '#ffffff';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-  } else {
-    //this is normal vertical
-    //const grd = ctx.createLinearGradient(0, 0, canvas.width, 0);
-    //note: Lexie likes this one below
-    //const grd = ctx.createLinearGradient(0, 0, 200, 200);
+  if (onItems.length > 0) {
+    //normal vertical would be (0, 0, canvas.width, 0);
     const grd = ctx.createLinearGradient(0, 0, 200, 0);
 
     onItems.forEach((item, index) => {
@@ -145,17 +125,10 @@ function renderLinearGradientCanvas(){
 function renderDiagonalGradientCanvas(){
   console.log('rendering diagonal gradient canvas - choice is ' + choice);
 
-  const canvas = document.getElementById('colorCanvas');
-  const ctx = canvas.getContext('2d');
-  //ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const {canvas, ctx, onItems} = getContext();
 
-  const onItems = Object.values(items).filter(item => item.on);
-
-  if (onItems.length === 0) {
-    ctx.fillStyle = '#ffffff';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-  } else {
-    const grd = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+  if (onItems.length > 0) {
+    const grd = ctx.createLinearGradient(0, 0, 200, 200);
 
     onItems.forEach((item, index) => {
       grd.addColorStop(index / onItems.length, item.color);
@@ -167,36 +140,119 @@ function renderDiagonalGradientCanvas(){
 }
 
 function renderRadialGradientCanvas(){
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
+  const {canvas, ctx, onItems} = getContext();
 
-  // Create a radial gradient
-  // The inner circle is at x=110, y=90, with radius=30
-  // The outer circle is at x=100, y=100, with radius=70
-  
-
-  const onItems = Object.values(items).filter(item => item.on);
-
-  if (onItems.length === 0) {
-    ctx.fillStyle = '#ffffff';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-  } else {
-    const gradient = ctx.createRadialGradient(110, 90, 30, 100, 100, 70);
+  if (onItems.length > 0) {
+    const gradient = ctx.createRadialGradient(150, 70, 20, 150, 80, 90);
 
     onItems.forEach((item, index) => {
-      gradient.addColorStop(index, item.color);
+      gradient.addColorStop(index / onItems.length, item.color);
     });
 
     ctx.fillStyle = gradient;
-    ctx.fillRect(20, 20, 160, 160);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
   }
+}
 
-  // // Add three color stops
-  // gradient.addColorStop(0, "pink");
-  // gradient.addColorStop(0.9, "white");
-  // gradient.addColorStop(1, "green");
+function renderConicGradientCanvas(){
+  const {canvas, ctx, onItems} = getContext();
 
-  // // Set the fill style and draw a rectangle
-  // ctx.fillStyle = gradient;
-  // ctx.fillRect(20, 20, 160, 160);
+  if (onItems.length > 0) {
+    const gradient = ctx.createConicGradient(170, 150, 80);
+
+    onItems.forEach((item, index) => {
+      gradient.addColorStop((index / onItems.length), item.color);
+    });
+
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
+function renderCircleParticlesCanvas(){
+  //todo sarah split this function so it can be called with different sizes for different buttons 
+  //"dandelion" 800 "circle particles" 101 "suncatcher" 1000 - maybe?
+  console.log('rendering circle particles canvas');
+
+  const {canvas, ctx, onItems} = getContext();
+  //ctx.reset();
+  //ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  if (onItems.length > 0) {
+    
+    ctx.globalAlpha = 0.5;
+
+    let particlesArray = [];
+    const size = 101;
+    let colors = generateParticleColors(size);
+
+    generateParticles(size, colors);
+    setSize();
+    animate();
+
+    function generateParticleColors(size){
+      const onColors = onItems.map(item => item.color);
+      let colors = [];
+
+      while (colors.length < size){
+        colors = [...colors, ...onColors];
+      }
+      console.log(`generateParticleColors colors has ${colors.length} and size is ${size}`);
+
+      return colors;
+    }
+
+    function generateParticles(amount, colors) {
+      //console.log(`particlesArray first ${particlesArray.length} and adding ${amount}`);
+      for (let i = 0; i < amount; i++) {
+        particlesArray[i] = new Particle(
+          innerWidth / 2,
+          innerHeight / 2,
+          50,
+          colors[i],
+          0.01,
+        );
+      }
+    }
+
+    function setSize() {
+      canvas.height = innerHeight;
+      canvas.width = innerWidth;
+    }
+
+    function Particle(x, y, particleTrailWidth, strokeColor, rotateSpeed) {
+      this.x = x;
+      this.y = y;
+      this.particleTrailWidth = particleTrailWidth;
+      this.strokeColor = strokeColor;
+      this.theta = Math.random() * Math.PI * 2;
+      this.rotateSpeed = rotateSpeed;
+      this.t = Math.random() * 150;
+
+      this.rotate = () => {
+        const ls = {
+          x: this.x,
+          y: this.y,
+        };
+        this.theta += this.rotateSpeed;
+        this.x = innerWidth / 2 + Math.cos(this.theta) * this.t;
+        this.y = innerHeight / 2 + Math.sin(this.theta) * this.t;
+        ctx.beginPath();
+        ctx.lineWidth = this.particleTrailWidth;
+        ctx.strokeStyle = this.strokeColor;
+        ctx.moveTo(ls.x, ls.y);
+        ctx.lineTo(this.x, this.y);
+        ctx.stroke();
+      };
+    }
+
+    function animate() {
+      requestAnimationFrame(anim);
+
+      ctx.fillStyle = "rgba(0,0,0,0.05)";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      particlesArray.forEach((particle) => particle.rotate());
+    }
+  }
 }
